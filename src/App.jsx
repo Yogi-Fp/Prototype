b543cc8677209482c88d717ea61fbcf19f666060
Yogi Fernando <yogifernando678@gmail.com>
2025-10-20 17:46:31 +0700

    Update fitur X

--- src/App.jsx
+++ src/App.jsx
@@ -42,16 +42,6 @@ export default function App() {
       return [];
     }
   });
-   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tambah" element={<Tambah />} />
-        <Route path="/edit/:id" element={<Edit />} />
-      </Routes>
-    </Router>
-  );
-}
   const [view, setView] = useState("today");
   const [isAdmin, setIsAdmin] = useState(() => {
     return localStorage.getItem("attendance_is_admin") === "1";
