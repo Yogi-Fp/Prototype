04e7ec940e593c238181b5f3bcbe09a616612b29
Yogi Fernando <yogifernando678@gmail.com>
2025-10-20 16:35:44 +0700

    Update fitur X

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Tambah from "./Tambah";
 import Edit from "./Edit";
@@ -8,12 +8,12 @@ import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/tambah" element={<Tambah />} />
         <Route path="/edit/:name" element={<Edit />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
